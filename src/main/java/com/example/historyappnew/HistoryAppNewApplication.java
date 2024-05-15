f310c17cfc9c8d3c77bf1f3f31788b4efe0b73a2
pako8101 <pako810129@gmail.com>
2024-05-15 20:24:41 +0300

    Initial commit

---
+++ src/main/java/com/example/historyappnew/HistoryAppNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.historyappnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoryAppNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoryAppNewApplication.class, args);
+	}
+
+}
