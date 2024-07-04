f013f044aeb6ad3bd37164936605f5dc36792e31
pako8101 <pako810129@gmail.com>
2024-07-04 23:53:34 +0300

    email scheduling

---
+++ src/main/java/HistoryAppGradleSecurity/emailScheduler/EmailScheduler.java
@@ -0,0 +1,51 @@
+package HistoryAppGradleSecurity.emailScheduler;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@EnableScheduling
+@Service
+public class EmailScheduler {
+
+
+    private MailSender mailSender;
+
+    @Value("${email_username}") private String sender;
+
+
+
+
+    @Scheduled(fixedRate = 12 * 60 * 60 * 1000) // 12 часа в милисекунди
+    public void sendScheduledEmails() {
+        sendEmail("alexcheto2013@abv.bg","Scheduled Email",
+                "This is a scheduled email sent every 12 hours.");
+
+
+    }
+
+    private String sendEmail(String to,String subject,String text) {
+     try {
+        SimpleMailMessage message =
+                new SimpleMailMessage();
+        message.setFrom(sender);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        mailSender.send(message);
+
+        return "Email sent successfully";
+    }
+       catch (
+    Exception e) {
+
+
+        return  "Error while sending mail!!!";
+    }
+}
+
+
+}
